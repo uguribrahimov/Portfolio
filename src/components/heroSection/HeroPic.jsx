d928dc665a172c1642ee31b498bc8cf222b16957
uguribrahimov <uguribrahimov04@gmail.com>
2025-04-06 12:10:45 +0400

    1

--- src/components/heroSection/HeroPic.jsx
+++ src/components/heroSection/HeroPic.jsx
@@ -12,8 +12,8 @@ const HeroPic = () => {
       className=" h-full flex items-center justify-center "
     >
       <img
-        src="../../public/images/me1.png"
-        alt="Abdelrahman Qassem"
+        src="/images/me1.png"
+        alt="Ibrahimov Ugur"
         className="max-h-[450px] w-auto"
       />
 
