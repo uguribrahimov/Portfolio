036074e32dbdbd8d4ae668491356bd4437857ce8
uguribrahimov <uguribrahimov04@gmail.com>
2025-04-06 12:35:47 +0400

    zz

--- src/components/subHeroSection/SubHeroMain.jsx
+++ src/components/subHeroSection/SubHeroMain.jsx
@@ -1,8 +1,8 @@
 const SubHeroMain = () => {
   return (
-    <div className="w-full border-y bg-brown border-lightGrey text-lightGrey flex justify-around uppercase xl:text-4xl md:text-2xl text-xl py-8 flex-row items-center gap-4">
-      <p className="">Fast Learner</p>
-      <p className="">Team Work</p>
+    <div className="w-full border-y bg-brown border-lightGrey text-lightGrey flex justify-around uppercase text-sm sm:text-center md:text-2xl xl:text-4xl py-8 flex-row items-center gap-4 ">
+      <p>Fast Learner</p>
+      <p>Team Work</p>
       <p>Details Master</p>
     </div>
   );
